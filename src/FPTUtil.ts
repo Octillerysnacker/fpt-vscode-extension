395d37ef67a79f68f6be6cd9fecdc1745a11ab69
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-26 18:40:12 -0500

    Added createRandomObject utility function

--- src/FPTUtil.ts
+++ src/FPTUtil.ts
@@ -1,5 +1,26 @@
 import { isArray } from "util";
-
+import {Random} from "random-js";
 export function isTArray<T>(objectArray: any, comparer: (object: any) => object is T):objectArray is T[]{
     return isArray(objectArray) && objectArray.every(comparer);
+}
+type looseObject = {[key: string]: any};
+
+export function createRandomObject(random: Random, maxNestingLevel: number,minProperties: number, maxProperties:number){
+    let object : any;
+    let b = 1000;//Random integer bounds. Totally arbritrary.
+    let randomStringMaxLengths = 15;
+    let options:((() => boolean) | (() => number) | (() => string) | (() => looseObject))[] = [
+        random.bool,
+        () => random.integer(-b,b),
+        () => random.string(random.integer(0,randomStringMaxLengths)),
+        () => random.real(-b,b),
+        () => {return {};}];
+    let result : boolean | number | string | looseObject = random.pick(options)();
+    if(result === {} && maxNestingLevel > 0){
+        let propertyCount = random.integer(minProperties, maxProperties);
+        for(let i = 0;i<propertyCount;i++){
+            result[random.string(random.integer(1,randomStringMaxLengths))] = createRandomObject(random,maxNestingLevel - 1,minProperties,maxProperties);
+        }
+    }
+    return result;
 }
