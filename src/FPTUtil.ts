1d19ec02c9fa20e835a23fb4a3c7132009dc4adf
Octillerysnacker <octillerysnacker@gmail.com>
2019-08-27 09:34:40 -0500

    Added options to createRandomObject

--- src/FPTUtil.ts
+++ src/FPTUtil.ts
@@ -5,16 +5,44 @@ export function isTArray<T>(objectArray: any, comparer: (object: any) => object 
 }
 type looseObject = { [key: string]: any };
 
-export function createRandomObject(random: Random, maxNestingLevel: number, minProperties: number, maxProperties: number) {
+export function createRandomObject(
+    random: Random, 
+    maxNestingLevel: number, 
+    minProperties: number, 
+    maxProperties: number,
+    allowedObjects : {
+        makeBooleans?: boolean,
+        makeIntegers?: boolean,
+        makeStrings?: boolean,
+        makeFloats?: boolean,
+        makeObjects?: boolean,
+    } = {
+        makeBooleans: true,
+        makeIntegers: true,
+        makeStrings: true,
+        makeFloats: true,
+        makeObjects: true
+    }) {
     let object: any;
     let b = 1000;//Random integer bounds. Totally arbritrary.
     let randomStringMaxLengths = 15;
-    let options: ((() => boolean) | (() => number) | (() => string) | (() => looseObject))[] = [
-        random.bool,
-        () => random.integer(-b, b),
-        () => random.string(random.integer(0, randomStringMaxLengths)),
-        () => random.real(-b, b),
-        () => { return {}; }];
+    let options: ((() => boolean) | (() => number) | (() => string) | (() => looseObject))[] = [];
+    if(allowedObjects.makeBooleans){
+        options.push(random.bool);
+    }
+    if(allowedObjects.makeFloats){
+        options.push(() => random.real(-b, b));
+    }
+    if(allowedObjects.makeIntegers){
+        options.push(() => random.integer(-b, b));
+    }
+    if(allowedObjects.makeObjects){
+        options.push(() => { return {}; });    
+    }
+    if(allowedObjects.makeStrings){
+        options.push(() => random.string(random.integer(0, randomStringMaxLengths)));
+    }
+
     let result: boolean | number | string | looseObject = random.pick(options)();
     if (result === {} && maxNestingLevel > 0) {
         let propertyCount = random.integer(minProperties, maxProperties);
