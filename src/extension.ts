a0be318721f7e4a2d4de0e8bc4a8da20758813ad
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-04 11:40:35 -0500

    changed commands to userfacing

--- src/extension.ts
+++ src/extension.ts
@@ -30,10 +30,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let core = new FPTAppMapper(new DotNetCoreFPTApp(promisify(exec),context.asAbsolutePath(path.join("core","FPT.Core.MainApp.dll")),context.extensionPath));
 	context.subscriptions.push(
-		vscode.commands.registerCommand("fpt.extension.getLevels", async () =>{
+		vscode.commands.registerCommand("fpt.extension.userFacing.getLevels", async () =>{
 			vscode.window.showInformationMessage(JSON.stringify(await core.getLevels()));
 		}),
-		vscode.commands.registerCommand("fpt.extension.openLevel", async()=>{
+		vscode.commands.registerCommand("fpt.extension.userFacing.openLevel", async()=>{
 			let levels = core.getLevels();
 			let user = await vscode.window.showInputBox({prompt:"Enter username"});
 			let items = (await levels).map((value:ILevel):vscode.QuickPickItem => {
@@ -47,7 +47,7 @@ export function activate(context: vscode.ExtensionContext) {
 				vscode.window.showInformationMessage(await core.openLevel(item.detail,user));
 			}
 		}),
-		vscode.commands.registerCommand("fpt.extension.getInstructions",async () =>{
+		vscode.commands.registerCommand("fpt.extension.userFacing.getInstructions",async () =>{
 			let levels = await core.getLevels();
 			let items = levels.map((value:ILevel):vscode.QuickPickItem => {
 				return {
@@ -60,6 +60,21 @@ export function activate(context: vscode.ExtensionContext) {
 				let path = core.getInstructions(item.detail);
 				let panel = vscode.window.createWebviewPanel("FPT","Instructions", vscode.ViewColumn.Two); 
 				panel.webview.html = Marked.parse((await vscode.workspace.openTextDocument(await path)).getText());
+			}
+		}),
+		vscode.commands.registerCommand("fpt.extension.userFacing.verify",async () =>{
+			let user = await vscode.window.showInputBox({prompt: "Enter a user to verify:"});
+			let levels = await core.getLevels();
+			let items = levels.map((value:ILevel):vscode.QuickPickItem =>{
+				return {
+					label:value.Name,
+					detail:value.Id
+				};
+			});
+			let item = await vscode.window.showQuickPick(items,{canPickMany : false,placeHolder: "Choose a level for instructions"});
+			if(item !== undefined && item.detail !== undefined && user !== undefined){
+				let result = await core.verify(item.detail,user);
+				vscode.window.showInformationMessage(JSON.stringify(result));
 			}
 		})
 	);
