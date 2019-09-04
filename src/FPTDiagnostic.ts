7601d3296f70db6833ba78abdb5deb82caf7b082
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-03 22:02:25 -0500

    Added FPTDiagnostic type guards

--- src/FPTDiagnostic.ts
+++ src/FPTDiagnostic.ts
@@ -4,8 +4,24 @@ export interface FPTDiagnostic{
     readonly Start : FileLocation;
     readonly End : FileLocation;
 }
-export enum DiagnosticKind {Error, Warning, Info}
+export function isFPTDiagnostic(object : any): object is FPTDiagnostic{
+    return typeof object.Message === "string" &&
+        isDiagnosticKind(object.Kind) &&
+        isFileLocation(object.Start) &&
+        isFileLocation(object.End);
+        
+}
+export enum DiagnosticKind {Error = 0, Warning = 1, Info = 2}
+export function isDiagnosticKind(object : any): object is DiagnosticKind{
+    return object === DiagnosticKind.Error ||
+        object === DiagnosticKind.Info ||
+        object === DiagnosticKind.Warning;
+}
 export interface FileLocation {
     readonly Line : number;
     readonly Position : number;
+}
+export function isFileLocation(object : any):object is FileLocation{
+    return typeof object.Line === "number" &&
+        typeof object.Position === "number";
 }
--- src/VerifierResult.ts
+++ src/VerifierResult.ts
@@ -1,6 +1,11 @@
-import { FPTDiagnostic } from "./FPTDiagnostic";
+import { FPTDiagnostic, isFPTDiagnostic } from "./FPTDiagnostic";
+import { isTArray } from "./FPTUtil";
 
 export interface VerifierResult{
     readonly Success : boolean;
     readonly Diagnostics : FPTDiagnostic[];
+}
+export function isVerifierResult(object : any): object is VerifierResult{
+    return typeof object.Success === "boolean" &&
+        isTArray<FPTDiagnostic>(object.Diagnostics,isFPTDiagnostic);
 }
