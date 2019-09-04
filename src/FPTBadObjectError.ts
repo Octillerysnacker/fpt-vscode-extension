57c2c51bb488c3e92a9e9e545093db6ad4a9f673
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-03 21:44:08 -0500

    Made FPTBadObjectError not generic

--- src/FPTBadObjectError.ts
+++ src/FPTBadObjectError.ts
@@ -1,4 +1,4 @@
-export class FPTBadObjectError<T> extends Error {
+export class FPTBadObjectError extends Error {
     public readonly badObject: any;
     constructor(badObject: any, message?: string) {
         super(message);
