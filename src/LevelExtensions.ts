c10f96294ed021b9cf3bd5938106b1c3cb043c9f
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-03 21:41:48 -0500

    Forgot to remove InitializerFilepath from LevelExtensions

--- src/LevelExtensions.ts
+++ src/LevelExtensions.ts
@@ -5,7 +5,6 @@ export function createRandomLevel(random: Random, fieldLengths: number): ILevel 
         Id: random.string(fieldLengths),
         Name: random.string(fieldLengths),
         VerifierFilepath: random.string(fieldLengths),
-        InitializerFilepath: random.string(fieldLengths),
         InstructionsFilepath: random.string(fieldLengths),
         FolderFilepath: random.string(fieldLengths)
     };
