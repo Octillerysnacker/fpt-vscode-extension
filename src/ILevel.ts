edbfa4d47b0975ef41ae626e6f1aaf4239e2687e
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-03 21:27:31 -0500

    Updated ILevel signature

--- src/ILevel.ts
+++ src/ILevel.ts
@@ -4,13 +4,11 @@ export interface ILevel {
     readonly FolderFilepath: string;
     readonly VerifierFilepath: string;
     readonly InstructionsFilepath: string;
-    readonly InitializerFilepath: string;
 }
 export function isILevel(object: any): object is ILevel {
     return object.Id !== undefined &&
         object.Name !== undefined &&
         object.FolderFilepath !== undefined &&
         object.VerifierFilepath !== undefined &&
-        object.InstructionsFilepath !== undefined &&
-        object.InitializerFilepath !== undefined;
+        object.InstructionsFilepath !== undefined;
 }
--- src/Level.ts
+++ src/Level.ts
@@ -6,13 +6,11 @@ export class Level implements ILevel {
     FolderFilepath: string;
     VerifierFilepath: string;
     InstructionsFilepath: string;
-    InitializerFilepath: string;
-    constructor(id?: string, name?: string, folderFilepath?: string, instructionsFilepath?: string, initializerFilepath?: string, verifierFilepath?: string) {
+    constructor(id?: string, name?: string, folderFilepath?: string, instructionsFilepath?: string, verifierFilepath?: string) {
         this.Id = id || "";
         this.Name = name || "";
         this.FolderFilepath = folderFilepath || "";
         this.VerifierFilepath = verifierFilepath || "";
         this.InstructionsFilepath = instructionsFilepath || "";
-        this.InitializerFilepath = initializerFilepath || "";
     }
 }
